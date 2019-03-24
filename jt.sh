94631e0dd4d63d05a6734371dd1aa69245091368
Ali <38077025+ATahma@users.noreply.github.com>
2019-03-24 14:21:45 -0400

    jupyter themes

---
+++ jt.sh
@@ -0,0 +1,2 @@
+# shell command to run for configuring jupyter themes -- must have the font anonymous installed
+jt -t gruvboxd -f anonymous -fs 12 -nfs 11 -nf anonymous -tf anonymous -tfs 14 -dfs 12 -ofs 12 -cellw 1400 -lineh 150
